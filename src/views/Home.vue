4ed8298d77e3aae3e23a4dc17dfd2fcb7ae47a57
Quinten Verhelst <quinten.verhelst@student.kdg.be>
2021-07-04 23:21:23 +0200

    😉 Add http request example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,15 @@
 <template>
   <h1>Home</h1>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {
+  async mounted(): Promise<void>{
+    const response = await this.$http.get('https://jsonplaceholder.typicode.com/todos')
+    console.log(response.data);
+  }
+}
+</script>
